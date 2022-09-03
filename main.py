7a478dd0a30c6f40663d27c0f78891841164cddd
Evgenii Lebedev <ptyz2000@gmail.com>
2022-09-03 14:04:35 +0300

    90% there

--- main.py
+++ main.py
@@ -1,62 +1,46 @@
-import os
-import subprocess
-from pywinauto import Application, keyboard
-import pathlib
-from argparse import ArgumentParser
+import glob
 import logging
+import pathlib
+import shutil
 import time
-from mss.windows import MSS as mss
+from argparse import ArgumentParser
+
 import pyautogui
-import pygetwindow as gw
+from mss.windows import MSS
+from pywinauto import Application
+
+import pyuac
 
 
 def test(exe, out):
-    with mss() as sct:
+    with MSS() as sct:
         logging.info('executable at: ' + exe)
         logging.info('output location at: ' + str(out))
+        fraps = Application().start(r'C:\Fraps\fraps.exe')
         app = Application(backend='win32').start(exe)
-        print(app.process)
-        print(app)
-        # window = app.top_window_()
-        # time.sleep(1)
-        # logging.info('game started with PID: ' + str(pid))
-        # time.sleep(30)
-        # window.set_focus()
-        keyboard.send_keys('{ENTER}')
-        # time.sleep(1)
-        # pyautogui.press('enter')
-        # sct.shot(output=str(out/"start.png"))
-        # # TODO: start fps log
-        # pyautogui.keyDown('w')
-        # time.sleep(10)
-        # pyautogui.keyUp('w')
-        # # TODO: stop fps log
-        # sct.shot(output=str(out/"end.png"))
-        # # TODO: save fps log
-        # pyautogui.press('escape')
-        # time.sleep(1)
-        # pyautogui.press('down')
-        # time.sleep(1)
-        # pyautogui.press('down')
-        # time.sleep(1)
-        # pyautogui.press('enter')
-        # time.sleep(1)
-        # pyautogui.press('down')
-        # time.sleep(1)
-        # pyautogui.press('down')
-        # time.sleep(1)
-        # pyautogui.press('enter')
-
-        # pyautogui.keyDown('enter')
-        # time.sleep(10)
-        # filename = sct.shot(output=str(out/"1.png"))
-        # print(filename)
+        logging.info('started with PID: ' + str(app.process))
+        time.sleep(60)
+        pyautogui.press('enter')
+        sct.shot(output=str(out/"start.png"))
+        pyautogui.press('f11')
+        pyautogui.keyDown('w')
+        time.sleep(10)
+        pyautogui.keyUp('w')
+        pyautogui.press('f11')
+        sct.shot(output=str(out/"end.png"))
+        for file in glob.glob(r'C:\Fraps\Benchmarks\*'):
+            shutil.move(file, out)
+        app.kill()
+        fraps.kill()
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('executable', help='path to executable file', )
-    parser.add_argument('-o', '--output', dest='output', default=None, help='output directory', type=pathlib.Path)
-    args = parser.parse_args()
-    logging.basicConfig(filename=str(args.output/'log.txt'), encoding='utf-8', level=logging.DEBUG)
-    test(args.executable, args.output)
+    if not pyuac.is_user_admin():
+        pyuac.run_as_admin()
+    else:
+        parser = ArgumentParser()
+        parser.add_argument('executable', help='path to executable file', )
+        parser.add_argument('-o', '--output', dest='output', default=None, help='output directory', type=pathlib.Path)
+        args = parser.parse_args()
+        logging.basicConfig(filename=str(args.output/'log.txt'), encoding='utf-8', level=logging.DEBUG)
+        test(args.executable, args.output)
